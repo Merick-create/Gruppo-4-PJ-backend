6ca9356adf92909c7842f8e4e90202c3950a7c2e
Merick-create <ric05bras@gmail.com>
2025-09-17 12:04:58 +0200

    dto movimenti

---
+++ backend/src/api/Movimenti/movimenti-dto.ts
@@ -0,0 +1,8 @@
+export class MovimentiDTO{
+    ContoCorrenteId!:string;
+    dataCreazione!:Date;
+    impoto!:number;
+    saldo!:number;
+    CategoriaMovimentoid!:string;
+    descrizione!:string;
+}
