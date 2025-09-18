cd3701e1fe81dd99360c96dc2bbcaf1126efa87e
Merick-create <ric05bras@gmail.com>
2025-09-18 10:16:21 +0200

    router category

--- backend/src/api/route.ts
+++ backend/src/api/route.ts
@@ -2,8 +2,10 @@ import {Router} from 'express';
 import movimentiRouter from './Movimenti/movimenti-router';
 import authRouter from './auth/auth.router';
 import contoCorrenteRouter from './Conto-Corrente/conto-corrente-router';
+import categorieRouter from './Categorie-Movimenti/categorie-router';
 
 const router = Router();
 router.use('/movimenti', movimentiRouter);
+router.use('/categorie', categorieRouter);
 router.use(authRouter);
 export default router;
