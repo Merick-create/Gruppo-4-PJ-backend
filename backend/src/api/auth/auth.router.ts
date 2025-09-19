1b0a33c1239b5d41530db731d98229f833daeac1
Lorenzo <lorenzo.forner@allievi.itsdigitalacademy.com>
2025-09-19 11:05:30 +0200

    add email send & confirm

--- backend/src/api/auth/auth.router.ts
+++ backend/src/api/auth/auth.router.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { add, login, updPssw } from "./auth.controller";
+import { add, confirm, login, updPssw } from "./auth.controller";
 import { validate } from "../../lib/validation.middleware";
 import { AddUserDTO } from "./auth.dto";
 import { isAuthenticated } from "../../lib/auth/auth.middleware";
@@ -9,5 +9,6 @@ const router = Router();
 router.post('/register', validate(AddUserDTO), add);
 router.post('/login', login);
 router.post('/updatePassword', isAuthenticated, updPssw);
+router.post('/confirmEmail', confirm);
 
 export default router;
