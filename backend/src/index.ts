b78990a3d91f40836a6e3dabe34cb3e97669aba5
Merick-create <ric05bras@gmail.com>
2025-09-24 16:35:05 +0200

    ssusu

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,9 +2,14 @@ import 'reflect-metadata';
 import { createServer } from 'http';
 import app from './app';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/gruppo4';
 
 mongoose.set('debug', true);
-mongoose.connect('mongodb://localhost:27017/gruppo4')
+mongoose.connect(MONGO_URI)
     .then(_ => {
         createServer(app).listen(3000, () => {
             console.log('Server listening on port 3000');
